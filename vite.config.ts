ee7c8903cc4750741755223a4fb0cc355fd650a7
Jose Galvez <josegalvez@azpa.com.py>
2026-05-07 11:14:35 -0300

    version apk

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/your-solo-notes/",
   plugins: [
     tsConfigPaths(),
     tanstackRouter({ target: "react", autoCodeSplitting: true }),
